f0b697e07b797191dc3a57c0c8d270fc8b3fc102
TralahM <musyoki.tralah@students.jkuat.ac.ke>
2019-01-30 22:06:43 +0300

    update code

--- get.py
+++ get.py
@@ -9,9 +9,22 @@ headers = { 'X-Auth-Token': API  }
 connection.request('GET', '/v2/competitions/PL/matches', None, headers )
 Response = json.loads(connection.getresponse().read().decode())
 print(Response.keys())
-print(len(Response['matches']))
-print((Response['matches'][0].keys()))
 
-for i in range(len(Response['matches'])):
-    print(Response['matches'][i]['utcDate'].split('T')[0],Response['matches'][i]['homeTeam']['name'],'',Response['matches'][i]['awayTeam']['name'])
+#for i in range(len(Response['matches'])):
+#    print(Response['matches'][i]['utcDate'].split('T')[0],Response['matches'][i]['homeTeam']['name'],'',Response['matches'][i]['awayTeam']['name'])
 
+for k in Response.keys():
+    print(k)
+    if isinstance(Response[k],dict):
+        for l in Response[k].keys():
+            print(k,'-->',l)
+            if isinstance(Response[k][l],dict):
+                for j in Response[k][l].keys():
+                    print(k,'-->',l,'-->',j)
+    if isinstance(Response[k],list):
+        if isinstance(Response[k][0],dict):
+            for m in Response[k][0].keys():
+                print(k, '[0] -->', m)
+                if isinstance(Response[k][0][m],dict):
+                    for n in Response[k][0][m].keys():
+                        print(k, '[0] -->',m,'-->',n)
