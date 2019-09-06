0c225a6b56ec72e735e85468decb5fdbe05e753f
TralahM <musyoki.tralah@students.jkuat.ac.ke>
2019-09-06 23:51:30 +0300

    Script to auto install dependencies

---
+++ get_dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Automating the dependency installation for the script...."
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install python3 python3-dev python3-pip git vim openssl -y
+git config --global user.email "musyoki.tralah@students.jkuat.ac.ke"
+git config --global user.name "TralahM"
+alias pip=pip3
+alias python=python3
+pip3 install sqlalchemy MySQLdb pandas numpy
+echo "Done with dependencies..."
+echo "\n Setting up crontab....."
+export EDITOR=vim
+cp fetch_matches.py ~/remote_mysqldb_updater.py
+crontab -e
