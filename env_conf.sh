f217be8f347ab4e98b438b03ff41cace9aa6afcd
Psycadelik <francisadrian9@hotmail.com>
2019-09-15 19:21:08 +0000

    add env conf on cron

---
+++ env_conf.sh
@@ -0,0 +1,4 @@
+PIP_PYTHON_PATH=/usr/bin/python3
+PATH=/root/.local/share/virtualenvs/football-4s91idYk/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+VIRTUAL_ENV=/root/.local/share/virtualenvs/football-4s91idYk
+PIP_PYTHON_PATH=/usr/bin/python3
